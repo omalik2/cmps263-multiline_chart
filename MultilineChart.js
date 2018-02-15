1c09f6da421cc45283c47ec1dfbdde3a4bc9d968
Osman Malik <o.malik22@gmail.com>
2018-02-15 11:03:20 -0800

    One in one out of shown on label click is happening

--- MultilineChart.js
+++ MultilineChart.js
@@ -7,16 +7,16 @@ var canvas = {w: 960, h: 510},
 
 // Add svg element to body of html
 var svg = d3.select("body").append("svg")
-    // Define width and heights of svg
-    .attr("class", "canvas")
-    .attr("width", canvas.w )
-    .attr("height", canvas.h),
+// Define width and heights of svg
+.attr("class", "canvas")
+.attr("width", canvas.w )
+.attr("height", canvas.h),
     //.style.display = "block"
     //.style.margin = "auto",
     // Add a group element that will contain the actual plot 
     g = svg.append("g")
-        // Align the group element so that padding is created from the top left.
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+// Align the group element so that padding is created from the top left.
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 svg.style.display = "block";
 svg.style.margin = "auto";
@@ -45,9 +45,9 @@ var x = d3.scaleTime().range([0,width]),
    data. The x and y functions extract the scaled x and y coordinates
    from the input data for each row. */
 var line = d3.line()
-    .curve(d3.curveNatural)
-    .x(function(d) {/* return scaled year */ return x(d.year); })
-    .y(function(d) {/* return scaled energy */return y(d.energy); });
+.curve(d3.curveNatural)
+.x(function(d) {/* return scaled year */ return x(d.year); })
+.y(function(d) {/* return scaled energy */return y(d.energy); });
 
 // gridlines on bottom axis with scaling of x
 function make_x_gridlines() {
@@ -59,14 +59,20 @@ function make_y_gridlines() {
 }
 
 // Open the preprocessed BRICSU csv
-d3.csv("BRICSdata.csv", type, function(error, data) {
+d3.csv("EPC_2000_2010_t.csv", type, function(error, data) {
     if (error) throw error;
-    
+
+    var shown = ["Brazil", "Russia", "India", "China", "South Africa", "United States"];
+    var hidden = data.columns.slice(1);
+    hidden = hidden.filter(function(el) {
+        return !shown.includes(el);
+    });
+
     /* Create 2D array from the 1D array of countries (= columns.slice(1))
        with the first element, 'id' as the country and 2nd element 'values' as
        as a *function* which maps each row in 'data' to an array of year and energy
        that is returned to 'values' */
-    var countries = data.columns.slice(1).map(function(id) {
+    var countries = shown.map(function(id) {
         return {
             id: id,
             values: data.map(function(d) {
@@ -79,8 +85,10 @@ d3.csv("BRICSdata.csv", type, function(error, data) {
        The input domain depends on the data and here it the range of maximum
        year to minimum year (2010 to 2000). The unnamed function defines which
        value to look at in each row of data. */
-    x.domain(d3.extent(data, function(d) { return d.year; }));
-    
+    var abc =d3.extent(data, function(d) {return d.year; });
+    console.log(abc);
+    x.domain(abc);
+
     /* Define the input domain of the linear scale the is of length 'height'.
        The input domain will be used for the left axis and so varies from the
        minimum energy value detected in the input data to the maximum energy
@@ -89,47 +97,47 @@ d3.csv("BRICSdata.csv", type, function(error, data) {
         d3.min(countries, function(c) { return d3.min(c.values, function(d) { return d.energy; }); }),
         d3.max(countries, function(c) { return d3.max(c.values, function(d) { return d.energy; }); })
     ]);
-    
+
     /* The domain for the data series are the country names */
     z.domain(countries.map(function(c) { return c.id; }));
-    
+
     // Adding new group that is x axis with axis title
     g.append("g")
         .attr("class", "axis axis--x")
-        // Move the axis to the bottom of the container 'g'
+    // Move the axis to the bottom of the container 'g'
         .attr("transform", "translate(0," + height + ")")
-        // Create the axis with the scaling declared by x
+    // Create the axis with the scaling declared by x
         .call(d3.axisBottom(x))
-        // Add text element
+    // Add text element
         .append("text")
-            .attr("text-anchor", "middle") // Center text
-            .attr("x", width/2)            // translate to middle of x-axis
-            .attr("y", margin.bottom)      // move it below x-axis
-            .attr("fill", "#000")          // Color it black
-            .attr("font-weight", "bold")   // Bold text
-            .text("Year");                 // Set text to display
-    
+        .attr("text-anchor", "middle") // Center text
+        .attr("x", width/2)            // translate to middle of x-axis
+        .attr("y", margin.bottom)      // move it below x-axis
+        .attr("fill", "#000")          // Color it black
+        .attr("font-weight", "bold")   // Bold text
+        .text("Year");                 // Set text to display
+
     // Adding new group that is the y axis with axis title
     g.append("g")
         .attr("class", "axis axis--y")
-        // Create the axis with the scaling declared by yy
+    // Create the axis with the scaling declared by yy
         .call(d3.axisLeft(y))
-        // Add text element
+    // Add text element
         .append("text")
-            .attr("text-anchor", "middle")      // Center text
-            .attr("x", -height/2)               // Place in middle of y-axis
-            .attr("y", -margin.left*4/5)        // Place behind y-axis
-            .attr("transform", "rotate(270)")   // Vertical text
-            .attr("fill", "#000")               // Text color is black
-            .attr("font-weight", "bold")        // Bold font
-            .text("Million BTUs Per Person");   // Text content
+        .attr("text-anchor", "middle")      // Center text
+        .attr("x", -height/2)               // Place in middle of y-axis
+        .attr("y", -margin.left*4/5)        // Place behind y-axis
+        .attr("transform", "rotate(270)")   // Vertical text
+        .attr("fill", "#000")               // Text color is black
+        .attr("font-weight", "bold")        // Bold font
+        .text("Million BTUs Per Person");   // Text content
 
     // add new group that is the X gridlines
     g.append("g")
         .attr("class", "grid")
-        // Move the grid lines so that they brought down to x axis
+    // Move the grid lines so that they brought down to x axis
         .attr("transform", "translate(0," + height + ")")
-        // Make the gridlines
+    // Make the gridlines
         .call(make_x_gridlines()
               // x.ticks().length returns number of ticks in x-axis data
               // Do not want clutter so use half number of ticks
@@ -142,11 +150,11 @@ d3.csv("BRICSdata.csv", type, function(error, data) {
               // will be added to the graph
               .tickFormat("")
              )
-    
+
     // add the Y gridlines
     g.append("g")
         .attr("class", "grid")
-        // Make the gridlines
+    // Make the gridlines
         .call(make_y_gridlines()
               // y.ticks().length returns number of ticks in y-axis data
               // Do not want clutter so use half number of ticks
@@ -159,65 +167,199 @@ d3.csv("BRICSdata.csv", type, function(error, data) {
               // will be added to the graph
               .tickFormat("")
              )
-    
+
     var country = g.selectAll(".country")
-        .data(countries)
-        // Create placeholder for each country in countries and append group
-        .enter().append("g")
-        // define class of each group created
-        .attr("class", "country");    
-    
+    .data(countries)
+    // Create placeholder for each country in countries and append group
+    .enter().append("g")
+    // define class of each group created
+    .attr("class", "country");
+
     // Add a the svg element path to the group: country
     var path = country.append("path")
-        .attr("class", "line")
-        // 'd' is the draw attribute of path and takes coordinates 
-        .attr("d", function(d) {
-            return line(d.values); 
-        })
-        // stroke style is defined by the z scale, which assigns
-        // each data series with an color specific to that ordinal
-        .style("stroke", function(d) { return z(d.id); });
-    
+    .attr("class", "line")
+    // 'd' is the draw attribute of path and takes coordinates
+    .attr("d", function(d) {
+        return line(d.values);
+    })
+    // stroke style is defined by the z scale, which assigns
+    // each data series with an color specific to that ordinal
+    .style("stroke", function(d) { return z(d.id); });
+
     path
-        // Length of the dashed line. We want 1 long dash
+    // Length of the dashed line. We want 1 long dash
         .attr("stroke-dasharray", function() {
-            return path.node().getTotalLength();
-        })
-        // Length of the space between dashed lines. This has to be equal to
-        // length of dashed line if we want a growing effect from left to right 
+        return path.node().getTotalLength();
+    })
+    // Length of the space between dashed lines. This has to be equal to
+    // length of dashed line if we want a growing effect from left to right
         .attr("stroke-dashoffset", function(){
-            return path.node().getTotalLength();
-        })
+        return path.node().getTotalLength();
+    })
         .transition()   // Animate with transitiontran
-            .duration(2000)         // Duration of transition
-            .ease(d3.easeLinear)    // Constant speed of animation
-            .attr("stroke-dashoffset", 0);  // When to stop 
-    
+        .duration(2000)         // Duration of transition
+        .ease(d3.easeLinear)    // Constant speed of animation
+        .attr("stroke-dashoffset", 0);  // When to stop
+
     // Add text the country group
     country.append("text")
-        // Set the bound data on this element. No need to .data as label is static
+    // Set the bound data on this element. No need to .data as label is static
         .datum(function(d) { 
-            return {id: d.id, value: d.values[d.values.length - 1]};
-        })
-        // Move element to appropriate location on chart 
+        return {id: d.id, value: d.values[d.values.length - 1]};
+    })
+    // Move element to appropriate location on chart
         .attr("transform", function(d) {
-            return "translate(" + x(d.value.year) + "," + y(d.value.energy) + ")";
-        })
-        // Shift element further to the right so that there is no overlap
+        return "translate(" + x(d.value.year) + "," + y(d.value.energy) + ")";
+    })
+    // Shift element further to the right so that there is no overlap
         .attr("x", 3)
-        // Shift element down so that it is centered with line
+    // Shift element down so that it is centered with line
         .attr("dy", "0.35em")
-        // Set font style
+    // Set font style
         .style("font", "10px sans-serif")
-        // Add text (which country it is) to label
-        .text(function(d) { return d.id; });
+    // Add text (which country it is) to label
+        .text(function(d) { return d.id; })
+        .on("click", function (d){
+            console.log("onclick entered for: " + d.id);
+
+
+            // Push clicked country into hidden list
+            hidden.push(d.id);
+            // Remove country at index in shown list
+            shown.splice(shown.indexOf(d.id), 1);
+            // Add new country to shown list from front of hidden list
+            shown.push(hidden.shift());
+
+            var countries_ = shown.map(function(id) {
+                return {
+                    id: id,
+                    values: data.map(function(d) {
+                        return {year: d.year, energy: d[id]};
+                    })
+                };
+            });
+
+            console.log(countries_);
+
+            x.domain(d3.extent(data, function(d) { return d.year; }));
+            y.domain([
+                d3.min(countries_, function(c) { return d3.min(c.values, function(d) { return d.energy; }); }),
+                d3.max(countries_, function(c) { return d3.max(c.values, function(d) { return d.energy; }); })
+            ]);
+
+            /* The domain for the data series are the country names */
+            z.domain(countries_.map(function(c) { return c.id; }));
+
+            var country_ = g.selectAll(".country")
+                        .data(countries_);
+
+            console.log(country_);
+
+            var exit = country_.exit()
+                exit
+                .remove(function(){
+                    console.log("Exiting");
+                });
+
+            // Create placeholder for each country in countries and append group
+            country_.enter()
+                .append("g")
+                .attr("class", "country")
+                .append("path")
+                    .attr("class", "line")
+                    // 'd' is the draw attribute of path and takes coordinates
+                    .attr("d", function(d) {
+                        console.log("New Path Created for: " + d.id);
+                        return line(d.values);
+                    })
+                    // stroke style is defined by the z scale, which assigns
+                    // each data series with an color specific to that ordinal
+                    .style("stroke", function(d) { return z(d.id); })
+                    // Length of the dashed line. We want 1 long dash
+                    .attr("stroke-dasharray", function(d) {
+                        //console.log("Cunt who has no path: " + d.id);
+                        var path_ = country_.select("path");
+                        return path_.node().getTotalLength();
+                    })
+                // Length of the space between dashed lines. This has to be equal to
+                // length of dashed line if we want a growing effect from left to right
+                    .attr("stroke-dashoffset", function(){
+                        var path_ = country_.select("path");
+                        return path_.node().getTotalLength();
+                    })
+                    .transition()   // Animate with transitiontran
+                    .duration(2000)         // Duration of transition
+                    .ease(d3.easeLinear)    // Constant speed of animation
+                    .attr("stroke-dashoffset", 0)  // When to stop
+                // Add text the country group
+                country_.enter().append("text")
+                // Set the bound data on this element. No need to .data as label is static
+                // Move element to appropriate location on chart
+                    .attr("transform", function(d) {
+                        var last = d.values[d.values.length-1];
+                        console.log("Added Text for " + d.id);
+                        return "translate(" + x(last.year) + "," + y(last.energy) + ")";
+                    })
+                    // Shift element further to the right so that there is no overlap
+                    .attr("x", 3)
+                    // Shift element down so that it is centered with line
+                    .attr("dy", "0.35em")
+                    // Set font style
+                    .style("font", "10px sans-serif")
+                    .text(function(d) { return d.id; });
+
+
+
+        // Update old, repurposed lines
+        country_.select("path")
+            .attr("d", function(d){
+                return line(d.values);
+            })
+            .style("stroke", function(d) { return z(d.id); })
+        // Length of the dashed line. We want 1 long dash
+            .attr("stroke-dasharray", function(d) {
+                var path_ = country_.select("path");
+                return path_.node().getTotalLength();
+            })
+        // Length of the space between dashed lines. This has to be equal to
+        // length of dashed line if we want a growing effect from left to right
+            .attr("stroke-dashoffset", function(){
+                var path_ = country_.select("path");
+                return path_.node().getTotalLength();
+            })
+            .transition()   // Animate with transitiontran
+            .duration(2000)         // Duration of transition
+            .ease(d3.easeLinear)    // Constant speed of animation
+            .attr("stroke-dashoffset", 0);  // When to stop
+
+            // Update text the country group
+            country_.select("text")
+                .data(countries_)
+            // Move element to appropriate location on chart
+                .attr("transform", function(d) {
+                    var last = d.values[d.values.length-1];
+                    console.log("Updating Text for " + d.id);
+                    return "translate(" + x(last.year) + "," + y(last.energy) + ")";
+                })
+                // Shift element further to the right so that there is no overlap
+                    .attr("x", 3)
+                // Shift element down so that it is centered with line
+                    .attr("dy", "0.35em")
+                // Set font style
+                    .style("font", "10px sans-serif")
+                    .text(function(d) { return d.id; });
+
+    });
 });
 
 // Type function to declare formatting of each input row 'd'. Used with .csv import
 function type(d, _, columns) {
-    d.year = parseTime(d.year); // Parse year as time
+    // Parse the first column as Date
+    console.log(d.year);
+    d.year = parseTime(d.year);
+    // Parse the entries in column 1 and onwards as numerical
     for (var i = 1, n = columns.length; i < n; ++i){
-        // For each column other than year, parse the (energy) values as numerical
+        // For each column other than Country, parse the (energy) values as numerical
         d[columns[i]] = +d[columns[i]];  
     }
     // Return the formatted row
